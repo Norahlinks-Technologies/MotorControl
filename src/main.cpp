355ab79ce6946fc1fcc3121ca553761510254aa4
bigfavour-2022 <norahlinkstech@gmail.com>
2022-10-27 18:51:58 +0100

    Test for all channels

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ int dir= 7;
 int vr= 6;
 int sig = 5;
 
-int ch6 = 9;
-int ch5 = 10;
-int ch4 = 8;
+int ch6 = 8;
+int ch5 = 9;
+int ch4 = 10;
 int ch3 = 11;
 int ch2 = 12;
 int ch1 = 13;
@@ -40,31 +40,32 @@ void setup()
 
 void loop() 
 {
-  ch1Data = pulseIn(ch1, HIGH, 250000);
-  ch2Data = pulseIn(ch2, HIGH, 250000);
-  ch3Data = pulseIn(ch3, HIGH, 250000);
-  ch4Data = pulseIn(ch4, HIGH, 250000);
-  ch5Data = pulseIn(ch5, HIGH, 250000);
-  ch6Data = pulseIn(ch6, HIGH, 250000);
+  ch1Data = pulseIn(ch1, HIGH, 25000);
+  ch2Data = pulseIn(ch2, HIGH, 25000);
+  ch3Data = pulseIn(ch3, HIGH, 25000);
+  ch4Data = pulseIn(ch4, HIGH, 25000);
+  ch5Data = pulseIn(ch5, HIGH, 25000);
+  ch6Data = pulseIn(ch6, HIGH, 25000);
 
 
-  Serial.print("channel 1");
+  Serial.print("channel 1:\t");
   Serial.println(ch1Data);
  
-  Serial.print("channel 2");
+  Serial.print("channel 2:\t");
   Serial.println(ch2Data);
 
-  Serial.print("channel 3");
+  Serial.print("channel 3:\t");
   Serial.println(ch3Data);
 
-  Serial.print("channel 4");
+  Serial.print("channel 4:\t");
   Serial.println(ch4Data);
 
-  Serial.print("channel 5");
+  Serial.print("channel 5:\t");
   Serial.println(ch5Data);
 
-  Serial.print("channel 6");
+  Serial.print("channel 6:\t");
   Serial.println(ch6Data);
 
-  _delay_ms(500);
+  _delay_ms(5000);
+  Serial.println("\n");
 }
