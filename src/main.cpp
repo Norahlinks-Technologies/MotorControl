38c4ef9b7a70398569db261597150b1a967f558d
bigfavour-2022 <norahlinkstech@gmail.com>
2022-10-27 16:48:46 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+int dir= 7;
+int vr= 6;
+int sig = 5;
+
+int ch6 = 9;
+int ch5 = 10;
+int ch4 = 8;
+int ch3 = 11;
+int ch2 = 12;
+int ch1 = 13;
+
+void setup() 
+{
+  pinMode(green, INPUT);
+  pinMode(yellow, INPUT);
+  pinMode(orange, INPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(vr, OUTPUT);
+  pinMode(sig, 1);
+
+}
+
+void loop() 
+{
+ 
+}
