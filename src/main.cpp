673d1f5ee96afbf71b19b0e06c5274a22bbdad52
bigfavour-2022 <norahlinkstech@gmail.com>
2022-10-27 17:27:55 +0100

    completed the motorControl code

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,60 @@ int ch3 = 11;
 int ch2 = 12;
 int ch1 = 13;
 
+long int ch6Data;
+long int ch5Data;
+long int ch4Data;
+long int ch3Data;
+long int ch2Data;
+long int ch1Data;
+
+
 
 void setup() 
+
 {
-  pinMode(green, INPUT);
-  pinMode(yellow, INPUT);
-  pinMode(orange, INPUT);
+  Serial.begin(9600);
+  pinMode(ch1, INPUT);
+  pinMode(ch2, INPUT);
+  pinMode(ch3, INPUT);
+  pinMode(ch4, INPUT);
+  pinMode(ch5, INPUT);
+  pinMode(ch6, INPUT);
+
+
   pinMode(dir, OUTPUT);
   pinMode(vr, OUTPUT);
-  pinMode(sig, 1);
+  pinMode(sig, OUTPUT);
 
 }
 
 void loop() 
 {
+  ch1Data = pulseIn(ch1, HIGH, 250000);
+  ch2Data = pulseIn(ch2, HIGH, 250000);
+  ch3Data = pulseIn(ch3, HIGH, 250000);
+  ch4Data = pulseIn(ch4, HIGH, 250000);
+  ch5Data = pulseIn(ch5, HIGH, 250000);
+  ch6Data = pulseIn(ch6, HIGH, 250000);
+
+
+  Serial.print("channel 1");
+  Serial.println(ch1Data);
  
+  Serial.print("channel 2");
+  Serial.println(ch2Data);
+
+  Serial.print("channel 3");
+  Serial.println(ch3Data);
+
+  Serial.print("channel 4");
+  Serial.println(ch4Data);
+
+  Serial.print("channel 5");
+  Serial.println(ch5Data);
+
+  Serial.print("channel 6");
+  Serial.println(ch6Data);
+
+  _delay_ms(500);
 }
